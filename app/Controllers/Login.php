b869372221f6c7dbe6d38317e6894fbb14724da1
shy <chentulin@163.com>
2021-03-10 18:00:47 +0800

    feat:开发环境开放手机验证码

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -30,9 +30,6 @@ class Login extends RestController
         $param = $this->request->getJSON();
         $model = new UserModel();
         $arr   = $model->where(['user_name' => $param->username])->find();
-        if (!RedisClient::getInstance()->hget('login_code', current($arr)['id'])) {
-            return $this->respondApi(['status' => 423, 'phone_num' => current($arr)['phone_num'], 'msg' => '请验证手机号码']);
-        }
         $ruleModel = new RuleModel();
         if (isset($param->password) && $param->password === current($arr)['password']) {
             $payload = [
@@ -51,10 +48,17 @@ class Login extends RestController
             $cache   = Services::cache();
             $cache->save($key, $authStr, 7200);
             RedisClient::getInstance()->setex('ci_' . $payload['exp'], 7200, 'token_expire_time');
+            $phone_num = current($arr)['phone_num'];
+            $key = 'login_code_' . $phone_num;
+            if (!RedisClient::getInstance()->exists($key) && (int)RedisClient::getInstance()->hget($key,$phone_num) !== 0) {
+                return $this->respondApi(['status' => 423, 'phone_num' => current($arr)['phone_num'], 'msg' => '请验证手机号码']);
+            }
             // 返回用户信息以方便存储到vuex里面去
             return $this->respondApi(['token' => $token, 'userInfo' => json_encode(current($arr))]);
+        }else{
+            return $this->respondApi(['code' => 201, 'msg' => '登录失败,请检查账号密码是否正确！']);
         }
-        return $this->respondApi(['code' => 201, 'msg' => '登录失败,请检查账号密码是否正确！']);
+
     }
 
     /**
